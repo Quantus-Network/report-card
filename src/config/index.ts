66379f4b4e34ba7d5ea928dc1cf2d8ac46aa87f2
Beast <idewagedebisma@gmail.com>
2025-09-19 14:08:01 +0800

    chore: update name and base site url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 const env = {
   SITE_NAME: "Quantus Report Card",
-  SITE_BASE_URL: "https://www.quantus.com",
+  SITE_BASE_URL: "https://q.day",
   ETHERSCAN_BASE_URL: "https://api.etherscan.io/v2/api?chainid=1",
 };
 
