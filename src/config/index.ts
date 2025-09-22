a4d06730e129a0e23e613ac0f7a08a445ed0bc04
Beast <idewagedebisma@gmail.com>
2025-09-22 17:46:19 +0800

    feat: finish updating to new styling in figma

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,8 @@ const env = {
   SITE_NAME: "Quantus Report Card",
   SITE_BASE_URL: "https://q.day",
   ETHERSCAN_BASE_URL: "https://api.etherscan.io/v2/api?chainid=1",
+  QUANTUS_SITE_URL: "https://www.quantus.com",
+  QUANTUS_RISK_CHECKER_URL: "https://www.quantus.com/quantum-risk-checker",
 };
 
 export default env;
